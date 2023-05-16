40910aef7b988ec21284281deb5425590e8a9fe0
Rifky Hamballi Syahfadilla <40071873+madara1902@users.noreply.github.com>
2023-05-16 11:38:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculate() {
+    setInterval(() =>{
+        let birthdate = new Date(document.getElementById('birthdate').value)
+
+        let now = new Date()
+        let ageInMs = now.getTime() - birthdate.getTime()
+
+        let ageIns = ageInMs / 1000 // to seconds
+        let ageInMins = ageIns / 60 // to minutes
+        let ageInHrs = ageInMins / 60 // to Hours
+        let ageInDays = ageInHrs / 24 // to Days
+        let ageInMonths = ageInDays / 30.4375 // to months
+        let ageInYears = ageInMonths / 12 // to Years
+
+        document.querySelector('#years').innerHTML = Math.floor(ageInYears)
+        document.querySelector('#months').innerHTML = Math.floor(ageInMonths % 12)
+        document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30.4375)
+        document.querySelector('#hours').innerHTML = Math.floor(ageInHrs % 24)
+        document.querySelector('#minutes').innerHTML = Math.floor(ageInMins % 60)
+        document.querySelector('#seconds').innerHTML = Math.floor(ageIns % 60)
+        document.querySelector('#seconds').style.borderBottom = '1px grey solid'
+    }, 1000)
+}
+
+function reset(){
+    window.location.reload()
+}
